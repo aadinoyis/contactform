f3db69349c2e22804e8754251fd2f1ec64580cf2
aadinoyis <abdulwahabadinoyis@gmail.com>
2022-12-08 22:30:04 +0000

    Add files via upload

---
+++ contactform/main.js
@@ -0,0 +1,22 @@
+let form = document.querySelector('form');
+let btn = document.querySelector('#msgbtn');
+let label = document.querySelector('.label');
+
+btn.onclick = (e) => {
+  e.preventDefault();
+  if (form.email.value !== '' && form.text.value !== '') {
+    form.submit();
+    label.textContent = 'Message sent';
+  } else {
+    label.textContent = 'email/message is empty';
+    label.style.color = 'red';
+  }
+}
+
+//COPYRIGHT
+cYear = new Date().getFullYear();
+
+let copy = document.querySelectorAll('#copy');
+for (let i = 0; i < copy.length; i++) {
+  copy[i].innerHTML = `&copy; ${cYear}`;
+};
